512088db97b37c39b31ad21244ecccdff59dfc55
athuljosep <70621782+athuljosep@users.noreply.github.com>
2024-02-05 15:54:37 -0800

    made more columns in app

--- MyDashApp_1.py
+++ MyDashApp_1.py
@@ -39,91 +39,324 @@ app.layout = dbc.Container([
         ], justify = "center", align = "center"),
 
     
+
     dbc.Row([
         
-
+        # First column
         dbc.Col([
             
+            html.Br(),
+            # First box
             # Database selection
             dcc.RadioItems(
                 id = 'database_selection',
                 labelStyle = {'display': 'block'},
                 options = [
-                    {'label' : "Our Database", 'value' : 1},
-                    {'label' : "Your Files", 'value' : 2}
+                    {'label' : " Our Database", 'value' : 1},
+                    {'label' : " Your Files", 'value' : 2}
                     ]  ,
                     value = 1,
-                    className = "mb-2"
+                    className = 'ps-4 p-3',
+                style={
+                    'width': '100%',
+                    'borderWidth': '1px',
+                    'borderStyle': 'solid',
+                    'borderRadius': '5px',
+                }
             ),
             
             html.Br(),
 
-            # Version selection
-            dcc.Dropdown(['8.0.0','9.0.0','22.0.0','23.0.0'], '9.0.0', id='version-selection'),
+            # Second box
+            html.Div([
+
+                # Upload IDF file
+                dcc.Upload([
+                    'Upload IDF file',
+                    #html.A('Select a File')
+                ],className = 'center', style={
+                    'width': '90%',
+                    'height': '40px',
+                    'lineHeight': '40px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin-left': '5%',
+                    'margin-top': '5%'
+                }),
+
+                # Upload EPW file
+                dcc.Upload([
+                    'Upload EPW file',
+                    #html.A('Select a File')
+                ],className = 'center', style={
+                    'width': '90%',
+                    'height': '40px',
+                    'lineHeight': '40px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin': '5%',
+                    
+                }),
+
+                
+                # Version selection
+                dbc.Stack([
+                    html.Label("Energy Plus Version:",
+                        className = 'text'),
+                    dcc.Dropdown(['8.0.0','9.0.0','22.0.0','23.0.0'], '9.0.0',
+                    id='version-selection',
+                    style = {
+                        'width':'60%',
+                        'margin-left':'8%'
+                    }),
+                ],direction="horizontal",style={
+                    'width': '90%',
+                    'margin': '5%',}),
+                
+
+
+
+                ],style={
+                    
+                    'borderWidth': '1px',
+                    'borderStyle': 'solid',
+                    'borderRadius': '5px',
+                    #'textAlign': 'center'
+            }),
+
+
+            
 
             html.Br(),
 
-            # Time-step selection
+            
             html.Div([
+
+                # Time-step selection
                 dbc.Stack([
                     html.Label("Time Step:",
                         className = 'text'), 
-                    daq.NumericInput(id='time-step', className = 'ms-auto',
-                        value=5,
-                    ),
+                    daq.NumericInput(id='time-step', 
+                        value=5,style = {
+                            #'width':'70%',
+                            'margin-left':'28%'
+                        }),
                 ],
                 direction="horizontal",
+                style={
+                    #'width': '90%',
+                    'margin': '5%',
+                }
                 ),
-            ] ),
+                
+                # Simulation reporting frequency selection
+                dbc.Stack([
+                    html.Label("Simulation Reporting Frequency:",
+                        className = 'text'), 
+                    dcc.Dropdown(['timestep','hourly','detailed','daily','monthly','runperiod','environment','annual'], 'timestep', id='simReportFreq-selection',
+                        style = {
+                            'width':'70%',
+                            'margin':'2%'
+                        }),
+                ],
+                direction="horizontal",
+                style={
+                    #'width': '90%',
+                    'margin': '5%',
+                }), 
 
+            ],style={
+                    'borderWidth': '1px',
+                    'borderStyle': 'solid',
+                    'borderRadius': '5px',
+            },),
 
-
+            
             
 
             
-
-            html.Br(),
 
             ], xs = 12, sm = 12, md = 4, lg = 4, xl = 4), # width = 12
         
         
-    
-        dbc.Col([
-            html.H3("Schedules",
-                    className = 'text-center')
-            ], xs = 12, sm = 12, md = 4, lg = 4, xl = 4),
 
+        # Second column
         dbc.Col([
-            html.H3("Building Type",
-                    className = 'text-center')
-            ], xs = 12, sm = 12, md = 4, lg = 4, xl = 4),
+
+            html.Br(),
+
+            # First box
+            html.Div([
+                html.H3("Schedules",
+                    className = 'text-center mt-1'),
+                #html.Br(),
+                html.H6("People",
+                    className = 'ms-2'),
+                html.H6("Equipment",
+                    className = 'ms-2'),
+                html.H6("Light",
+                    className = 'ms-2'),
+                html.H6("Exterior Light",
+                    className = 'ms-2'),
+                html.H6("Heat/Cool Setpoint",
+                    className = 'ms-2'),
+            ],style={
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+            },),
+            
+            html.Br(),
+
+            # Second box
+            html.Div([
+                html.Button('Generate Vaialbes', id = 'Button_1', 
+                        className = "btn btn-secondary btn-lg col-12",
+                        style={
+                            'width':'90%',
+                            'margin':'5%'
+                        },),
+
+                dcc.Dropdown(['var 1','var 2','var 3'], '', id='variable-selection',
+                        style = {
+                            'width':'95%',
+                            'margin-left':'2.5%',
+                            'margin-bottom':'5%'
+                        }),
+                
+            ],style={
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+            },),
+
+            html.Br(),
+
+            # Third box
+            html.Div([
+
+                dcc.Checklist([' Simulation Variables',' EIO',' IDF Object Records'], id='download-selection',
+                    style = {
+                        'width':'95%',
+                        'margin':'5%',
+
+                    }),
+                
+            ],style={
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+            },),
+
+            ], xs = 12, sm = 12, md = 4, lg = 4, xl = 4,
+            ),
+
+
+
+        # Third column
+        dbc.Col([
+            
+            html.Br(),
+
+            html.Div([
+
+                
+                # Building type selection
+                html.Label("Building Type",
+                        className = 'text-left ms-4 mt-1'),
+                dcc.Dropdown(['Commercial','Manufactured','Residential'], 'Commercial', id='buildingType-selection',
+                    style={
+                        'width': '95%',
+                        'margin-left': '2.5%',   
+                    }),
+
+                # IDF type selection
+                html.Label("IDF Type",
+                        className = 'text-left ms-4'),
+                dcc.Dropdown(['ASHRAE','IECC'], 'IECC', id='idfType-selection',
+                    style={
+                        'width': '95%',
+                        'margin-left': '2.5%',   
+                    }),
+
+                # IDF year selection
+                html.Label("IDF Year",
+                        className = 'text-left ms-4'),
+                dcc.Dropdown(['2012','2013','2015','2016','2018','2019'], '2013', id='idfYear-selection',
+                    style={
+                        'width': '95%',
+                        'margin-left': '2.5%',   
+                    }),
+
+                # Building selection
+                html.Label("Building",
+                        className = 'text-left ms-4'),
+                dcc.Dropdown(['ApartmentHighRise','Hospital','HotelLarge','HotelSmall','OfficeLarge','OfficeMedium','OfficeSmall'], 'OfficeSmall', id='building-selection',
+                    style={
+                        'width': '95%',
+                        'margin-left': '2.5%',   
+                    }),
+
+                # Location selection
+                html.Label("Location",
+                        className = 'text-left ms-4'),
+                dcc.Dropdown(['Albuquerque','Atlanta','Buffalo','Denver','ElPaso','Fairbanks','GreatFalls','Honululu','InternationalFalls','NewYork','PortAngeles','Rochester','SanDiego','Seattle','Tampa','Tucson'], 'Seattle', id='location-selection',
+                    style={
+                        'width': '95%',
+                        'margin-left': '2.5%',
+                        'margin-bottom': '3%',   
+                    },),
+
+                ],style={
+                    'borderWidth': '1px',
+                    'borderStyle': 'solid',
+                    'borderRadius': '5px',
+            }),
+
+            html.Br(),
+            
+            # Second box
+            html.Div([
+
+                html.Button('Generate Data', id = 'Button_1', 
+                        className = "btn btn-secondary btn-lg col-12",
+                        style={
+                            'width':'90%',
+                            'margin':'5%'
+                        },),
+
+                html.Button('Download Files', id = 'Button_1', 
+                        className = "btn btn-primary btn-lg col-12",
+                        style={
+                            'width':'90%',
+                            'margin-left':'5%',
+                            'margin-bottom':'5%'
+                        },),
+                
+            ],style={
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+            },),
+
+
+
+
+            ], xs = 12, sm = 12, md = 4, lg = 4, xl = 4,
+            ),
         
         ], justify = "center", align = "center"),
 
 
-    # Break Row
-    dbc.Row([
-        
-        dbc.Col([
-            
-            html.Br()
-            
-            ], width = 12),
-        
-        ]),  
+    
     
     
     # Row 
-    dbc.Row([
-        
-        dbc.Col([
-            
-            html.Button('Generate', id = 'Button_1', 
-                        className = "btn btn-primary btn-lg col-12") ,
-            
-            ], xs = 12, sm = 12, md = 12, lg = 12, xl = 12), # width = 12
-        
-        ], justify = "center", align = "center"),   
+  
 
   
     
@@ -140,7 +373,8 @@ app.layout = dbc.Container([
     State(component_id = 'database-selection', component_property = 'value'),
     State(component_id = 'version-selection', component_property = 'value'),
     State(component_id = 'time-step', component_property = 'value'),
-    
+    State(component_id = 'buildingType-selection', component_property = 'value'),
+
     prevent_initial_call = False)
 
 def CreateOutput():
